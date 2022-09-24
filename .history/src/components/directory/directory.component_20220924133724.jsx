f00005eb43319abf10d19f97fee446283b70938b
Wayne Yong <ymw0331@gmail.com>
2022-09-24 17:00:57 +0800

    fixed stylilng crashes

---
+++ .history/src/components/directory/directory.component_20220924133724.jsx
@@ -0,0 +1,14 @@
+import DirectoryItem from "../category-item/category-item.component";
+import "./direcotry.styles.scss";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <DirectoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
---
+++ .history/src/routes/navigation/navigation.component_20220924154112.jsx
@@ -0,0 +1,53 @@
+import { Fragment, useContext } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.contexts";
+import { signOutUser } from "../../utils/firebase/firebase.utils.js";
+
+import {
+  NavigationContainer,
+  NavLink,
+  NavLinksContaier,
+  LogoContainer,
+} from "./navigation.style.jsx";
+// import "./navigation.style.jsx";
+
+const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
+
+  return (
+    <Fragment>
+      <NavigationContainer>
+        <LogoContainer to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN IN
+            </Link>
+          )}
+          <CartIcon />
+        </div>
+        {/* if evaluated to be truthy, return CartDropdown */}
+        {isCartOpen && <CartDropdown />}
+      </NavigationContainer>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
