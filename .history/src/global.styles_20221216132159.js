a45243e33cdf513619c1a1a746082ee0fa502847
Wayne Yong <ymw0331@gmail.com>
2022-12-16 15:00:44 +0800

    added mobile responsive to styles

---
+++ .history/src/global.styles_20221216132159.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body {
+  font-family: 'Open Sans Condensed';
+  padding: 20px 40px;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+
+`
